e00f6e044e45f7f7dfcd370e404f1abeea96d7a0
mdb571 <r.mohammedbilal@gmail.com>
2020-05-30 13:53:53 +0530

    loadr1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-;
+
 $(document).ready(function(){
   $('.carousel').carousel({
     fullWidth: true,
   });
 });
 
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
+});
